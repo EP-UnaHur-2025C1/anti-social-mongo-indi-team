a8491974daf0e1bd425b8e856466c1ff5b8f00b2
SantiagoSolari <santiagosolariivan@gmail.com>
2025-06-11 17:42:55 -0300

    primeros avances

--- src/main.js
+++ src/main.js
@@ -1,1 +1,18 @@
-console.log("UnaHur - Anti-Social net");
+const express = require('express')
+require('dotenv').config()
+const connectDB = require('./config/db')
+const userRouter = require('./routes/userRoutes')
+
+const PORT = process.env.PORT || 3000
+const app = express()
+
+app.use(express.json())
+//rutas
+app.use('/usuarios', userRouter)
+
+//Conexion a DB
+connectDB()
+
+app.listen(PORT, () =>{
+    console.log(`la aplicacion esta corriendo en el puerto ${PORT}`)
+})
